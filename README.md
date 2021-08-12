a3e2cbe41821deb05486afb68f4cfd8a87045910
Juan Manuel Alvarez <naicigam@gmail.com>
2021-08-12 00:05:49 -0300

    PDF version.

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # Mastering Ethereum compiled
 
-This repo converts the [Mastering Ethereum](https://github.com/ethereumbook/ethereumbook) Asciidoc version to different formats.
+This repo compiles the [Mastering Ethereum](https://github.com/ethereumbook/ethereumbook) Asciidoc version into different formats.
+
+I do not own nor created the book.
 
 ## Supported formats
+
 - HTML
+- PDF
 
 ## Requirements
+
 - [Asciidoctor](https://asciidoctor.org/)
+- [Asciidoctor PDF](https://github.com/asciidoctor/asciidoctor-pdf)
+- [Asciidoctor Epub3](https://github.com/asciidoctor/asciidoctor-epub3)
+- [PDFtk command line](https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/)
 
 ## Usage
+
 - Checkout the [ebook repo](https://github.com/ethereumbook/ethereumbook).
 - Run: `./convert.sh <ethereum_book_dir>`.
+
+# License
+
+[Source](https://github.com/ethereumbook/ethereumbook#source-and-license)
+
+Creative Commons License
+Mastering Ethereum by [Andreas M. Antonopoulos, Gavin Wood](https://ethereumbook.info/) is licensed under a [Creative Commons Attribution-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-sa/4.0/).
--- convert.sh
+++ convert.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
+if [[ $# -ne 1 ]]; then
+    echo "ERROR: must pass the ethereumbook folder."
+    exit 2
+fi
+
 ETHEREUMBOOK_DIR=$1
+
+# --------------------------------------------------------------
+# Copy all ASCII doc and images
+
+ASCIIDOC_DIR=./asciidoc
+
+echo "Deleting old ASCII doc version..."
+rm -f -R $ASCIIDOC_DIR
+
+# Copying data
+mkdir -p $ASCIIDOC_DIR/images
+cp -R $ETHEREUMBOOK_DIR/images $ASCIIDOC_DIR
+cp -R $ETHEREUMBOOK_DIR/code $ASCIIDOC_DIR
+
+cp -R $ETHEREUMBOOK_DIR/*.asciidoc $ASCIIDOC_DIR
+rm $ASCIIDOC_DIR/book.asciidoc
+
+# --------------------------------------------------------------
+# HTML
+
 HTML_DIR=./html
 
 echo "Deleting old HTML version..."
@@ -9,7 +34,26 @@ rm -f -R $HTML_DIR
 echo "Generate HTML version..."
 mkdir -p $HTML_DIR/images
 cp -R $ETHEREUMBOOK_DIR/images $HTML_DIR
-asciidoctor -D $HTML_DIR $ETHEREUMBOOK_DIR/*.asciidoc
+asciidoctor -D $HTML_DIR $ASCIIDOC_DIR/*.asciidoc
 
-# -r asciidoctor-pdf
-# -r asciidoctor-epub3
+# --------------------------------------------------------------
+# PDF
+
+PDF_DIR=./pdf
+
+echo "Deleting old PDF version..."
+rm -f -R $PDF_DIR
+
+echo "Generate PDF version..."
+asciidoctor-pdf -D $PDF_DIR/chapters $ASCIIDOC_DIR/*.asciidoc
+pdftk $PDF_DIR/chapters/preface.pdf \
+        $PDF_DIR/chapters/0*.pdf \
+        $PDF_DIR/chapters/1*.pdf \
+        $PDF_DIR/chapters/appdx-*.pdf \
+        $PDF_DIR/chapters/github_contrib.pdf \
+        $PDF_DIR/chapters/glossary.pdf \
+    cat output $PDF_DIR/ethereumbook.pdf
+
+echo "Updating PDF bookmarks..."
+pdftk $PDF_DIR/ethereumbook.pdf dump_data > $PDF_DIR/bookmarks.info
+pdftk $PDF_DIR/ethereumbook.pdf update_info $PDF_DIR/bookmarks.info output $PDF_DIR/ethereumbook.pdf
